13e8a3fa245c29a479604b537dabf97ead0e47be
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-04-07 17:44:47 -0700

    stretch assignment attempt

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -8,14 +8,35 @@ const greenBlock = document.querySelector('div.block--green')
 const pinkBlock = document.querySelector('div.block--pink')
 const grayBlock = document.querySelector('div.block--gray')
 
+function addPositioningClass (element) {
+     element.classList.add('positioning')
+}
+addPositioningClass(redBlock)
+addPositioningClass(blueBlock)
+addPositioningClass(greenBlock)
+addPositioningClass(pinkBlock)
+addPositioningClass(grayBlock)
+
 console.log(blocks)
 
-let x = 0;
-let y = 0;
+// let x = 0;
+// let y = 0;
 
-blocks.forEach((block) => {
-     block.addEventListener('click', function (){  
+let moveRight = (block) => {
+     let x = 0;
+     block.addEventListener('mousedown', function (){  
           block.setAttribute('style', `--x: ${ x }px`)
-          return x += 10
+          return x += 20
 })
-})
+}
+
+moveRight(redBlock)
+moveRight(blueBlock)
+moveRight(greenBlock)
+moveRight(pinkBlock)
+moveRight(grayBlock)
+
+
+
+//set block to index 0
+//add 1 to index of all the others
